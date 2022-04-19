d47b8570857ac847ed2697aab0b2f24c3790f14c
BrianKheng <briankheng63@gmail.com>
2022-04-19 13:38:54 +0700

    Change variable name row -> i

--- Login.py
+++ Login.py
@@ -1,9 +1,9 @@
 import Data, FungsiBuatan, Cipher
 
 def validasi(username, password): # Validasi username & password dari variable global users
-    for row in range(FungsiBuatan.lenght(Data.users)):
-        if (Data.users[row][1] == username and Cipher.decrypt(Data.users[row][3], 4) == password):
-            return row
+    for i in range(FungsiBuatan.lenght(Data.users)):
+        if (Data.users[i][1] == username and Cipher.decrypt(Data.users[i][3], 4) == password):
+            return i
     return -1
 
 def login(): # Melakukan login, jika login tervalidasi maka mengembalikan idxUser
