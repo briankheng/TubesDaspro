e3367b5826c11bb43bfa2bdc7cc32205420b6cdc
jfayw <julianne20001@mail.unpad.ac.id>
2022-04-20 15:43:35 +0700

    revisi topup

--- TopUp.py
+++ TopUp.py
@@ -10,11 +10,15 @@ def topUpSaldo():
     # mencari username
     for i in range(FungsiBuatan.lenght(Data.user)):
         if(Data.user[i][1] == id):
-            if (Data.user[i][5] < saldo): # validasi nilai saldo
+            if (Data.user[i][5] + int(saldo) < 0): # validasi nilai saldo
                 print("Masukan tidak valid.")
             else:
                 Data.user[i][5] += int(saldo)
-                print("Top up berhasil. Saldo " + str(username) + "bertambah menjadi " + str(Data.user[i][5]) + ".")
+                if (int(saldo) >= 0):
+                    print ("Top up berhasil. Saldo " + str(username) + "bertambah menjadi " + str(Data.user[i][5]) + ".")
+                else:
+                    print ("Top up berhasil. Saldo " + str(username) + "berkurang menjadi " + str(Data.user[i][5]) + ".")
+                
             usernameValid = True
 
     # pesan error
