c803364e3398ab15df9211b3a4cc5dbd16c01bc7
Gevyndo <gevyndo18@gmail.com>
2022-04-20 16:30:13 +0700

    import

--- F09.py
+++ F09.py
@@ -1,30 +1,11 @@
 import Data, FungsiBuatan
 
-raw = open('FileEksternal/riwayat.csv', 'r').readlines()
-raw2 = open('FileEksternal/game.csv', 'r').readlines()
-def CSVParser (raw):
-    M = [["" for col in range(6)] for row in range(100)]
-    for i in range(len(raw)): # Fungsi len buat sendiri
-        idx = 0
-        temp = ""
-        for j in range(len(raw[i])): # Fungsi len buat sendiri
-            if(raw[i][j] == ';' or raw[i][j] == '\n'):
-                M[i][idx] = temp
-                temp = ""
-                idx += 1
-            else:
-                temp += raw[i][j]
-    return M
-riwayat = CSVParser(raw)
-game = CSVParser(raw2)
-print(riwayat)
-
 def asal():
     arr = []
     for i in range(100):
         for j in range(3,4):
-            if id==riwayat[i][j]:
-                arr +=[riwayat[i][0]]
+            if id==Data.riwayat[i][j]:
+                arr +=[Data.riwayat[i][0]]
     return arr
 
 def list_game():
@@ -36,8 +17,8 @@ def list_game():
             if index == len(asal()):
                 break
             for j in range(1):
-                if asal()[index] == game[i][0]:
-                    print(game[i][0],"|",game[i][1],"|",game[i][2],"|",game[i][3],"|",game[i][4])
+                if asal()[index] == Data.game[i][0]:
+                    print(Data.game[i][0],"|",Data.game[i][1],"|",Data.game[i][2],"|",Data.game[i][3],"|",Data.game[i][4])
                     index +=1
         return
 
