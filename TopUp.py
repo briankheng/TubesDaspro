913805786a70e8141721ea5e5f4eaa5f5917388a
BrianKheng <briankheng63@gmail.com>
2022-04-20 15:55:41 +0700

    Revisi fungsi TopUp.py

--- TopUp.py
+++ TopUp.py
@@ -8,21 +8,21 @@ def topUpSaldo():
     saldo = input("Masukkan saldo: ")
 
     # mencari username
-    for i in range(FungsiBuatan.lenght(Data.user)):
-        if(Data.user[i][1] == username):
-            if (Data.user[i][5] + int(saldo) < 0): # validasi nilai saldo
+    for i in range(FungsiBuatan.lenght(Data.users)):
+        if(Data.users[i][1] == username):
+            if (int(Data.users[i][5]) + int(saldo) < 0): # validasi nilai saldo
                 print("Masukan tidak valid.")
             else:
-                Data.user[i][5] += int(saldo)
+                Data.users[i][5] = int(Data.users[i][5]) + int(saldo)
                 if (int(saldo) >= 0):
-                    print ("Top up berhasil. Saldo " + str(username) + "bertambah menjadi " + str(Data.user[i][5]) + ".")
+                    print ("Top up berhasil. Saldo " + Data.users[i][2] + " bertambah menjadi " + str(Data.users[i][5]) + ".")
                 else:
-                    print ("Top up berhasil. Saldo " + str(username) + "berkurang menjadi " + str(Data.user[i][5]) + ".")
+                    print ("Top up berhasil. Saldo " + Data.users[i][2] + " berkurang menjadi " + str(Data.users[i][5]) + ".")
                 
             usernameValid = True
 
     # pesan error
     if (usernameValid == False):
-        print ("Username" + str(username) + "tidak ditemukan.")
+        print ("Username " + str(username) + " tidak ditemukan.")
 
 topUpSaldo()
