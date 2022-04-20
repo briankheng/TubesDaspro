bfac0eb556563f3053fcc283d789e4c04d9d0466
Gevyndo <gevyndo18@gmail.com>
2022-04-20 16:42:19 +0700

    f17

--- F17.py
+++ F17.py
@@ -1,11 +1,12 @@
 notExit = False
 
+def exit():
 while notExit :
    askSave= input (“save y/n”)
-   if askSave == y:
+   if askSave == "y" or askSave =="Y":
         save()
         notExit = True
-   elif askSave == n:
+   elif askSave == "n" or askSave =="N":
         notExit = True
    else: 
        continue
