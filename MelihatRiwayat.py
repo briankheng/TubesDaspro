5161af4a1ffc29fa95c587cfda3ccea7ca06f522
BrianKheng <briankheng63@gmail.com>
2022-04-20 19:13:32 +0700

    Sedikit revisi formating

--- MelihatRiwayat.py
+++ MelihatRiwayat.py
@@ -5,7 +5,8 @@ def riwayat(idxUser):
     for i in range(FungsiBuatan.lenght(Data.riwayat)):
         if(Data.users[idxUser][0] == Data.riwayat[i][3]):
             arr += [[Data.riwayat[i][0], Data.riwayat[i][1], Data.riwayat[i][2], Data.riwayat[i][4]]]
-    if(FungsiBuatan.lenght(arr)) == 0:
+    
+    if(FungsiBuatan.lenght(arr) == 0):
         print("Maaf, kamu tidak ada riwayat pembelian game. Ketik perintah beli_game untuk membeli.")
     else:
         print("Daftar game:")
