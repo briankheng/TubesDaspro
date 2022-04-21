1f53f426b7d04a2290128e7718236c49e09076fb
BrianKheng <briankheng63@gmail.com>
2022-04-21 10:46:31 +0700

    Ubah File Data.py, pembacaan file csv di Load.py

--- Data.py
+++ Data.py
@@ -1,7 +1,6 @@
-import FungsiBuatan
-
 # Variabel Global
-users = FungsiBuatan.CSVParser(open('FileEksternal/user.csv', 'r').readlines())
-games = FungsiBuatan.CSVParser(open('FileEksternal/game.csv', 'r').readlines())
-kepemilikan = FungsiBuatan.CSVParser(open('FileEksternal/kepemilikan.csv', 'r').readlines())
-riwayat = FungsiBuatan.CSVParser(open('FileEksternal/riwayat.csv', 'r').readlines())
+
+users = []
+games = []
+kepemilikan = []
+riwayat = []
