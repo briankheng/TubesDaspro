f96a5d6b284e5d9d0beb3134742dbbfce9dfb0d7
BrianKheng <briankheng63@gmail.com>
2022-04-21 19:18:16 +0700

    typecasting ke str

--- MelihatRiwayat.py
+++ MelihatRiwayat.py
@@ -7,10 +7,10 @@ def riwayat(idxUser):
             arr += [[Data.riwayat[i][0], Data.riwayat[i][1], Data.riwayat[i][2], Data.riwayat[i][4]]]
     
     if(FungsiBuatan.lenght(arr) == 0):
-        print("Maaf, kamu tidak ada riwayat pembelian game. Ketik perintah beli_game untuk membeli.")
+        print("Maaf, kamu tidak ada riwayat pembelian game. Ketik perintah buy_game untuk membeli.")
     else:
         print("Daftar game:")
         nomor = 1
         for i in arr:
-            print(str(nomor) + ". " + i[0] + " | " + i[1] + " | " + i[2] + " | " + i[3] + " |")
+            print(str(nomor) + ". " + str(i[0]) + " | " + str(i[1]) + " | " + str(i[2]) + " | " + str(i[3]) + " |")
             nomor += 1
