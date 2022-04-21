fd523af4c490805be006a5faea617e8528dea0bf
BrianKheng <briankheng63@gmail.com>
2022-04-21 19:41:01 +0700

    typecasting ke str

--- CariGameToko.py
+++ CariGameToko.py
@@ -9,11 +9,11 @@ def cariGameToko():
     nomor = 0
     print("Daftar game pada toko yang memenuhi kriteria: ")
     for i in range(1, FungsiBuatan.lenght(Data.games)):
-        if((Data.games[i][0].lower() == id.lower() or id == '') and (Data.games[i][1].lower() == nama or nama.lower() == '') 
-        and (Data.games[i][2].lower() == kategori.lower() or kategori == '') and (Data.games[i][3] == tahun or tahun == '') 
-        and (Data.games[i][4] == harga or harga == '')):
-            print(str(nomor+1)+'. '+Data.games[i][0]+' | '+Data.games[i][1]+' | '+Data.games[i][2]
-                +' | '+Data.games[i][3]+' | '+Data.games[i][4]+' | '+Data.games[i][5])
+        if((str(Data.games[i][0].lower()) == id.lower() or id == '') and (str(Data.games[i][1].lower()) == nama or nama.lower() == '') 
+        and (str(Data.games[i][2].lower()) == kategori.lower() or kategori == '') and (str(Data.games[i][3]) == tahun or tahun == '') 
+        and (str(Data.games[i][4]) == harga or harga == '')):
+            print(str(nomor+1)+'. '+str(Data.games[i][0])+' | '+str(Data.games[i][1])+' | '+str(Data.games[i][2])
+                +' | '+str(Data.games[i][3])+' | '+str(Data.games[i][4])+' | '+str(Data.games[i][5]))
             nomor += 1
     if(nomor == 0):
         print("Tidak ada game pada toko yang memenuhi kriteria")
