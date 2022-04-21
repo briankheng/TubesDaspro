2c2f00ee94ba3ff1963bedc721cae2c771cb6b0c
BrianKheng <briankheng63@gmail.com>
2022-04-21 15:57:59 +0700

    Fungsi Register auto save ke CSV

--- Register.py
+++ Register.py
@@ -1,4 +1,5 @@
-import Data, FungsiBuatan, Cipher
+import Data, FungsiBuatan, Cipher, os
+from Save import writeToCSV
 
 def validasiUsername(username): # Mengecek apakah username sudah terpakai atau belum
     for i in range (FungsiBuatan.lenght(Data.users)):
@@ -6,12 +7,14 @@ def validasiUsername(username): # Mengecek apakah username sudah terpakai atau b
             return False
     return True
 
-def register():
+def register(folder):
     nama = input("Masukan nama : ")
     username = input("Masukan username : ")
     password = input("Masukan password : ")
 
     if(validasiUsername(username)):
         Data.users += [[str(FungsiBuatan.lenght(Data.users)), username, nama, Cipher.encrypt(password, 4), "User", str(0)]]
+        writeToCSV(os.path.join(os.getcwd(), folder), "user.csv", Data.users)
+        print("\nUsername " + username + ' telah berhasil register ke dalam "Binomo".')
     else:
         print("\nUsername " + username + " sudah terpakai, silakan menggunakan username lain.")
